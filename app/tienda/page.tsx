9511d36a4640238f1dc87d5e38af1569e2493007
mopy69 <leonard67848953@gmail.com>
2026-06-14 07:53:45 -0400

    ci

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -2,8 +2,10 @@ export default function Tienda() {
   return (
     <div>
       <h1>Tienda</h1>
-      <p>Bienvenido a la tienda. Aquí encontrarás una variedad de productos para satisfacer tus necesidades.</p>
-      {/* Agrega aquí los componentes o elementos que desees mostrar en la tienda */}
+      <p>
+        Bienvenido a la tienda. Aquí encontrarás una variedad de productos para
+        satisfacer tus necesidades.
+      </p>
     </div>
   );
 }
